80cae5681eb251be8e1f3780173866016c180866
Ashish Malhotra <156682456+y-pred@users.noreply.github.com>
2025-04-02 14:12:31 +0530

    Add files via upload

---
+++ Chapter2/01_strings.py
@@ -0,0 +1,17 @@
+a = "Hello World"
+
+print(a[0]) # Prints first character
+print(a[-1]) #Prints last character
+print(a[0:5]) # Prints first 5 characters
+print(a[:-1]) # Prints all characters except last
+print(a[0:]) #Prints all characters
+
+print(len(a))
+print(a.upper())
+print(a.startswith("He"))
+print(a.endswith("ld"))
+
+a = 'Ash is a good boy\nbut not a bad \'boy\''
+
+
+print(a)
---
+++ Chapter2/02_fstring.py
@@ -0,0 +1,9 @@
+a = "Hello World"
+
+print(f"First character:{a[0]} and last character is {a[-1]}")
+
+letter = '''Dear XXX, 
+You are selected! 
+YYY '''
+
+print(letter.replace("XXX", "Ash").replace("YYY", "24 September 2050"))
